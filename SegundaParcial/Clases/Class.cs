16ef894ca159ab31a43d609a7cfb20094d54748d
famez <famez@HP255G8>
2024-10-21 19:02:04 -0600

    Agregar archivos de proyecto.

---
+++ SegundaParcial/Clases/Class.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBlazorApp.Models
+{
+    public class Product
+    {
+        public int InventoryNumber { get; set; }
+
+        [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
+        [StringLength(200, ErrorMessage = "El nombre no puede exceder los 200 caracteres.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "El departamento es obligatorio.")]
+        public string Department { get; set; }
+    }
+}
